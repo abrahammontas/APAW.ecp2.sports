d832353ab752971638ae7a105fba9fc6e06f3f11
Abraham Montas <abrahammontas@gmail.com>
2016-12-19 15:21:54 +0100

    Creacion de el UserResource y sus metodos

---
+++ src/main/java/es/upm/miw/apiArchitectureTheme/api/UserResource.java
@@ -0,0 +1,34 @@
+package es.upm.miw.apiArchitectureTheme.api;
+
+import es.upm.miw.apiArchitectureTheme.controllers.UserController;
+import es.upm.miw.apiArchitectureTheme.exceptions.InvalidThemeFieldException;
+import es.upm.miw.apiArchitectureTheme.exceptions.NotFoundThemeIdException;
+import es.upm.miw.apiArchitectureTheme.wrappers.UserListWrapper;
+
+public class UserResource {
+
+    private void validateField(String field) throws InvalidThemeFieldException {
+        if (field == null || field.isEmpty()) {
+            throw new InvalidThemeFieldException(field);
+        }
+    }
+    
+    // GET **/users
+    public UserListWrapper userList() {
+        return new UserController().themeList();
+    }
+
+    // POST **/users   body="nick,email"
+    public void createUser(String nick, String email) throws InvalidThemeFieldException {
+        this.validateField(nick);
+        this.validateField(email);
+        new UserController().createUser(nick, email);
+    }
+    
+    // GET **users/search?sport=*
+    public UserListWrapper searchBySport(String sport) throws NotFoundThemeIdException {
+        this.validateField(sport);
+        return new UserController().usersBySport(sport);
+    }
+
+}
